765436508e8198d1da607e5ce2a7de88e5f70305
phorvicheka <phorvicheka@yahoo.com>
2020-12-11 13:24:28 +0900

    Step 3 - Wrap Data Using LiveData
    https://codelabs.developers.google.com/codelabs/android-lifecycles/#3

---
+++ app/src/main/java/com/example/kkandroidlifecycles/step3/LiveDataTimerViewModel.kt
@@ -0,0 +1,42 @@
+package com.example.kkandroidlifecycles.step3
+
+import android.os.SystemClock
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import java.util.*
+
+/**
+ * A ViewModel used for the [com.example.kkandroidlifecycles.step3.ChronoActivity3].
+ */
+class LiveDataTimerViewModel : ViewModel() {
+    private val mElapsedTime = MutableLiveData<Long>()
+    private val mInitialTime: Long = SystemClock.elapsedRealtime()
+    private val timer: Timer = Timer()
+
+    // Will be used when step is completed
+    val elapsedTime: LiveData<Long>
+        get() = mElapsedTime
+
+    override fun onCleared() {
+        super.onCleared()
+        timer.cancel()
+    }
+
+    companion object {
+        private const val ONE_SECOND = 1000
+    }
+
+    init {
+        // Update the elapsed time every second.
+        timer.scheduleAtFixedRate(object : TimerTask() {
+            override fun run() {
+                val newValue = (SystemClock.elapsedRealtime() - mInitialTime) / 1000
+
+                // setValue() cannot be called from a background thread so post to main thread.
+                //TODO post the new value with LiveData.postValue()
+
+            }
+        }, ONE_SECOND.toLong(), ONE_SECOND.toLong())
+    }
+}
